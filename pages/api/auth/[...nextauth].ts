9899d6771e9014b2a9be0208140b68d4045a32e6
kemcet <kemalcetmensm@gmail.com>
2022-09-26 17:48:19 +0300

    connected to mongodb for user info and added google provider

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,16 +1,26 @@
+/**source: https://github.com/thatanjan/next-auth-yt **/
 import NextAuth from "next-auth"
 import GithubProvider from "next-auth/providers/github"
+import GoogleProvider from "next-auth/providers/google"
+import clientPromise from "../../../lib/mongodb";
+import { MongoDBAdapter } from '@next-auth/mongodb-adapter'
 
-const { GITHUB_ID = '', GITHUB_SECRET = ''} = process.env;
+const { GITHUB_ID = '', GITHUB_SECRET = '',GOOGLE_CLIENT_ID = '', GOOGLE_CLIENT_SECRET = ''} = process.env;
 
 export const authOptions = {
-  // Configure one or more authentication providers
   providers: [
     GithubProvider({
       clientId: GITHUB_ID,
       clientSecret: GITHUB_SECRET,
     }),
+    GoogleProvider({
+      clientId: GOOGLE_CLIENT_ID,
+      clientSecret: GOOGLE_CLIENT_SECRET,
+    }),
     // ...add more providers here
   ],
+  adapter: MongoDBAdapter(clientPromise),
+  secret: process.env.SECRET,
+  debug: true,
 }
 export default NextAuth(authOptions)
